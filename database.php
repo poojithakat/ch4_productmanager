ead5586b8cc97fa8188cdef4c9f9e3d0518f0349
poojithakat <pk457@njit.edu>
2017-06-29 10:46:06 -0400

    added database.php

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=pk457';
+        $username = 'pk457';
+	    $password = 'tCX2Wqcx';
+	        try {
+		        $db = new PDO($dsn, $username, $password);
+			    } catch (PDOException $e) {
+			            $error_message = $e->getMessage();
+				            include('database_error.php');
+					            exit();
+						        }
+							?>
